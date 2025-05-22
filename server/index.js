9e46268cfcc6e677d522796edfccc301a0833e15
sunnynand1 <sunnynand111@gmail.com>
2025-05-23 01:01:35 +0530

    update

--- server/index.js
+++
@@ -1,7 +0,0 @@
-// This is the main entry point for the backend application
-// It imports and starts the server defined in server.js
-
-require('./server');
-
-// This file is intentionally minimal as all the server logic is in server.js
-// This follows the principle of separation of concerns and makes the codebase more maintainable
